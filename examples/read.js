7bf9111b8479bdc28fc82db5dda77558b23332fb
Tim Cameron Ryan <id@timryan.org>
2013-09-11 14:37:16 -0400

    Remove firmata code directly from module

--- examples/read.js
+++ examples/read.js
@@ -1,7 +1,7 @@
 var hardware = require('hardware');
 
 console.log("Starting up S17005...");
-var climate = require('../').connect(hardware);
+var climate = require('../').connect(hardware.firmata('/dev/tty.usbmodem1411'), 2);
 
 climate.on('connected', function () {
   console.log("Connected to S17005");
@@ -9,8 +9,10 @@ climate.on('connected', function () {
   // Loop forever
   setImmediate(function loop () {
     climate.readTemperature('f', function (err, temp) {
-      console.log('Degrees:', temp.toFixed(4) + 'F');
-      setTimeout(loop, 300);
+      climate.readHumidity(function (err, humid) {
+        console.log('Degrees:', temp.toFixed(4) + 'F', 'Humidity:', humid.toFixed(4) + '%RH');
+        setTimeout(loop, 300);
+      });
     });
   });
 });
