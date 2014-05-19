e133c97a7260b399dca49166c85aaf65c6fecafd
Frijol <ifoundthemeaningoflife@gmail.com>
2014-05-18 22:09:40 -0700

    removed chip select from constructor function arguments

--- index.js
+++ index.js
@@ -53,16 +53,9 @@ var
 
 
 // Constructor
-function Climate (hardware, csn) {
-  /*
-  Args
-    hardware
-      Tessel port to use
-    csn
-      Chip select pin to use (active low). Wired to GPIO 1 on the module.
-  */
+function Climate (hardware) {
   this.hardware = hardware;
-  this.csn = csn || 1;
+  this.csn = 1;
   this._configReg = 0;
 
   // I2C object for address
